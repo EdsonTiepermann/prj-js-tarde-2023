71bbd9f2851115691051c97fd491207a4e19a078
IagoGonçalves22 <iago.g.goncalves@gmail.com>
2023-10-09 17:56:09 -0300

    validacao login

--- iago/login.js
+++ iago/login.js
@@ -1,13 +1,46 @@
 /** Aqui será a função de login **/
 
 function logar() {
-  var login = document.getElementById("login").value;
-  var senha = document.getElementById("password").value;
+  const login = document.getElementById("login").value;
+  const senha = document.getElementById("password").value;
 
-  if (login == "iago" && senha == "123") {
-    window.alert("sucesso");
-    window.location.href = "index.html";
-  } else {
-    alert("usuario ou senha incorretos");
-  }
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
+
+  fetch(`ws/loginJson.json`, options)
+    .then(function (response) {
+      response
+        .json()
+
+        .then(function (result) {
+          console.log(result);
+          let verifica = false;
+          let nome = "";
+
+          for (let i = 0; i < result.length; i++) {
+            if (login == result[i].login && senha == result[i].senha) {
+              nome = result[i].nome;
+              verifica = true;
+              break;
+            } else {
+              verifica = false;
+            }
+          }
+
+          if (verifica) {
+            window.open(`searchPage.html?nomeUsuario=${nome}`);
+          } else {
+            alert("Login ou senha inválido.");
+          }
+        });
+    })
+
+    .catch(function (e) {
+      console.log("Login ou senha inválidos: " + e.message);
+    });
 }
+
+// window.open('./filmes.html?nomeUsuario=${variavelUsuario}&senha=${variavelsenha}');
