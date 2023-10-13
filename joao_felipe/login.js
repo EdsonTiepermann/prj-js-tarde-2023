7c4d072e5ae5270c4a8f7ad5045e88f56c3826d4
joaofelipe02 <joaodecarvalhopradk@gmail.com>
2023-10-13 17:51:59 -0300

    prj 13/10/2023

--- joao_felipe/login.js
+++ joao_felipe/login.js
@@ -1,1 +1,23 @@
-const cep = document.querySelector('#username');
+function logar() {
+    let username = document.getElementById("exampleFormControlInput1").value
+    let resultado = parseInt(username)
+    
+    if (resultado = 2023) {
+        document.getElementById("res").innerHTML = "Seja bem vindo Joao" 
+    }
+
+    if (resultado = 123) {
+        document.getElementById("res").innerHTML = "Seja bem vindo Felipe" 
+    }
+
+    if (resultado = 123) {
+        document.getElementById("res").innerHTML = "Seja bem vindo Edson" 
+    }
+    
+    }
+
+    
+    
+
+
+
