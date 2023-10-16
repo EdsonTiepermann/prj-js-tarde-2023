d1d720344ed0f9b3a05af5cb51000bd38f585251
Cortelete <d.cortelete@gmail.com>
2023-10-16 17:52:21 -0300

    tuts

--- davi/login.js
+++ davi/login.js
@@ -3,28 +3,47 @@ var senha = document.querySelector('#senha')
 
 const submit = document.querySelector('#botao')
 
-submit.addEventListener('click', function(e) {
+submit.addEventListener('click', function (e) {
     e.preventDefault()
+    let searchl = login.value;
+    let searchs = senha.value;
 
     //alert("texto qualquer")
-    const options ={
+    const options = {
         method: 'get',
         mode: 'cors',
         cache: 'default'
-        
-    } 
-    fetch(`ws/loginJson.json`, options)
-    .then(function(response) {
-        
-        response.json()
 
-        .then(function(data) {
-            console.log(data);
+    }
+
+    fetch(`ws/loginJson.json`, options)
+        .then(function (response) {
+
+            response.json()
+
+                .then(function (data) {
+                    console.log(data);
+                    for (let i = 0; i < data.length; i++) {
+                        let login1 = data[i].login;
+                        let senha1 = data[i].senha;
+                        let nome = data[i].nome;
+                       
+                        if (searchl == login1 && searchs == senha1) {
+
+                            location.href = `sfilmes.html?nome=${nome}`
+                        } else {
+                            alert('Dados Incorretos')
+                        }
+                    }
+
+
+
+
+                })
         })
-    })
-    .catch(function(e) {
-        console.log('Error: ' + e.message);
-})
+        .catch(function (e) {
+            console.log('Error: ' + e.message);
+        })
 
 })
 //window.open(`./filmes.html?nomeUsu=${variavelUsuario}&senha=${variavelSenha}`);
