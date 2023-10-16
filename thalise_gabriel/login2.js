c1fcb7c03e5f9f2742ef5bab0bab0af8a92bb03f
gibsonsg002 <gabrielsantosgibson@gmail.com>
2023-10-16 17:48:35 -0300

    prj1610

--- thalise_gabriel/login2.js
+++ thalise_gabriel/login2.js
@@ -3,22 +3,27 @@ var senha = document.querySelector("#senha")
 const botao = document.querySelector("#botao")
 
 botao.addEventListener('click', function (e) {
-    alert('teste')
-
+    e.preventDefault()
     const options = {
         method: 'GET',
         mode: 'cors',
         cache: 'default',
     }
-
     fetch('ws/senhaJson.json', options)
-
         .then(function (response) {
             response.json()
-
                 .then(function (data) {
-
                     console.log(data)
+                    for (let i = 0; i < data.length; i++){
+                        if (data[i].senha == senha.value & data[i].login == login.value) {
+                            console.log(showData)
+                            window.open(`./pesquisa.html?login=${login}&thagab=`);
+                            break;
+                        } else {
+                            alert('Login ou senha incorretos')
+                            break;
+                       }
+                    }                               
                 })
         })
 
