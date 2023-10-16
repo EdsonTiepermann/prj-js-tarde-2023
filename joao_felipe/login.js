e6c448a37c39ce8b4681cb82d44cc0e49ae3fd84
joaofelipe02 <joaodecarvalhopradk@gmail.com>
2023-10-16 17:43:03 -0300

    prj js 16/10/2023

--- joao_felipe/login.js
+++ joao_felipe/login.js
@@ -1,23 +1,48 @@
-function logar() {
-    let username = document.getElementById("exampleFormControlInput1").value
-    let resultado = parseInt(username)
-    
-    if (resultado = 2023) {
-        document.getElementById("res").innerHTML = "Seja bem vindo Joao" 
+const username = document.querySelector('#exampleFormControlInput1');
+const password = document.querySelector('#senha');
+
+const botao = document.querySelector('#logar')
+
+botao.addEventListener('click', function (e){
+
+    let user = username.value;
+    let pass = password.value;
+
+    const Options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
     }
 
-    if (resultado = 123) {
-        document.getElementById("res").innerHTML = "Seja bem vindo Felipe" 
-    }
+    fetch(`ws/loginJson.json`, Options)
 
-    if (resultado = 123) {
-        document.getElementById("res").innerHTML = "Seja bem vindo Edson" 
-    }
-    
-    }
+    .then(function(response){
+        response.json()
 
-    
-    
+        .then(function(data){
+            let status = false;
+            let nome = "";
 
+            for (let index = 0; index < data.length; index++) {
+                if (data[index].login == user && data[index].senha == pass) {
+                    status = true;
+                    nome = data[index].nome;
+                    break;
+                } else {
+                    status = false;
+                }
+            }
 
+            if (status) {
+                alert('deu certo');
+                window.location.href = `pagInicial.html?nome=${nome}`;
+            } else {
+                alert('erro');
+            }
+        })
+    })
 
+    .catch(function (e) {
+        console.log('ERROR: ' + e.message);
+    })
+})
