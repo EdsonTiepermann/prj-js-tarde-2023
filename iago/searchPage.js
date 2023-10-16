79bbbb215f77f1760fd62132f3d6cfc3653d7d66
IagoGonçalves22 <iago.g.goncalves@gmail.com>
2023-10-16 17:52:06 -0300

    search params nome usuario e mouse pointer

--- iago/searchPage.js
+++ iago/searchPage.js
@@ -1,3 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+const nomeParams = urlParams.get("nomeUsuario");
+document.querySelector("#usuario").innerHTML += nomeParams;
+
+function limparInput() {
+  document.getElementById("filtro").value = "";
+}
+
 function showData(listaFilme) {
   document.querySelector("#inicial").style.display = "none";
 
@@ -5,50 +13,24 @@ function showData(listaFilme) {
     document.getElementById(
       "object"
     ).innerHTML += `<img id="${listaFilme.Search[index].Title}" class="qtdf" src=${listaFilme.Search[index].Poster} alt="Minha Figura"></img>`;
-    // + listaFilme.Search[index].Title
   }
   var elementos = document.querySelectorAll(".qtdf");
-  // alert('teste');
+
   [].forEach.call(elementos, function (el) {
     el.addEventListener("click", function (evento) {
-      // evento.preventDefault();
       console.log(el.id);
       window.location.href = `./poster.html?nomeFilme=${el.id}`;
     });
-    // whatever with the current node
   });
   console.log(elementos.length);
 }
 
-// criar ID para receber Search[index].Title
-
-function getFilmes(search) {
-  axios
-    .get(`http://www.omdbapi.com/?s=${search}&apikey=1b6aee61`)
-    .then(function (response) {
-      for (let index = 0; index < response.data.Search.length; index++) {
-        document.querySelector(
-          "#filme"
-        ).innerHTML += `<li class="teste" id="${response.data.Search[index].Title}"> ${response.data.Search[index].Title}<br></li>`;
-      }
-
-      var elementos = document.querySelectorAll(".poster");
-      forEach.call(elementos, function (elemento) {
-        elemento.addEventListener("click", function (evento) {
-          evento.preventDefaut();
-          window.location.href = `./poster.html?nome=${elemento.id}`;
-        });
-      });
-    });
-}
-
 document.querySelector("#buscar").addEventListener("click", function (e) {
   const filtro = document.querySelector("#filtro");
-
   const botao = document.querySelector("#buscar");
 
   let search = filtro.value.replace("-", "");
-  // getFilmes(search);
+
   const options = {
     method: "GET",
     mode: "cors",
@@ -59,11 +41,10 @@ document.querySelector("#buscar").addEventListener("click", function (e) {
     .then(function (response) {
       response.json().then(function (data) {
         showData(data);
+        limparInput();
       });
     })
     .catch(function (e) {
       console.log("Error: " + e.message);
     });
-
-  console.log(search);
 });
