2a7bb172140d6978abd0defef077efecd2e27a44
joaofelipe02 <joaodecarvalhopradk@gmail.com>
2023-10-20 17:52:09 -0300

    prj 20/10/2023

--- joao_felipe/login.js
+++ joao_felipe/login.js
@@ -3,7 +3,7 @@ const password = document.querySelector('#senha');
 
 const botao = document.querySelector('#logar')
 
-botao.addEventListener('click', function (e){
+botao.addEventListener('click', function (e) {
 
     let user = username.value;
     let pass = password.value;
@@ -16,33 +16,36 @@ botao.addEventListener('click', function (e){
 
     fetch(`ws/loginJson.json`, Options)
 
-    .then(function(response){
-        response.json()
+        .then(function (response) {
+            response.json()
 
-        .then(function(data){
-            let status = false;
-            let nome = "";
+                .then(function (data) {
+                    let status = false;
+                    let nome = "";
 
-            for (let index = 0; index < data.length; index++) {
-                if (data[index].login == user && data[index].senha == pass) {
-                    status = true;
-                    nome = data[index].nome;
-                    break;
-                } else {
-                    status = false;
-                }
-            }
+                    for (let index = 0; index < data.length; index++) {
+                        if (data[index].login == user && data[index].senha == pass) {
+                            status = true;
+                            nome = data[index].nome;
+                            break;
+                        } else {
+                            status = false;
+                        }
+                    }
 
-            if (status) {
-                alert('deu certo');
-                window.location.href = `pagInicial.html?nome=${nome}`;
-            } else {
-                alert('erro');
-            }
+                    if (status) {
+                        alert('deu certo');
+                        window.location.href = `pagInicial.html?nome=${nome}`;
+                    } else {
+                        alert('erro');
+                    }
+                })
         })
-    })
 
-    .catch(function (e) {
-        console.log('ERROR: ' + e.message);
-    })
+        .catch(function (e) {
+            console.log('ERROR: ' + e.message);
+            
+
+        })
+
 })
