262451629c1a592aafc1e8a618fc49dd28ae732c
joaofelipe02 <joaodecarvalhopradk@gmail.com>
2023-10-23 17:48:25 -0300

    prj 23/10/2023

---
+++ joao_felipe/paginaInicial.js
@@ -0,0 +1,45 @@
+let nameParameter = new URLSearchParams(window.location.search);
+let nomeUsuario = nameParameter.get("nome");
+
+document.querySelector('#saudacao').innerHTML = `Seja bem vindo ${nomeUsuario}`
+
+
+const filmePesquisa = document.querySelector("#procurar");
+const botao = document.querySelector("#buscar");
+
+
+botao.addEventListener('click', function (e) {
+    
+    let nomeFilme = filmePesquisa.value;
+
+    const Options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`http://www.omdbapi.com/?s=${nomeFilme}&apikey=eca49c5b`, Options)
+
+        .then(function(response) {
+            response.json()
+
+                .then(function (data) {
+                    
+                    for (let index = 0; index < data.Search.length; index++) {
+                        console.log(data.Search[index])
+
+                        document.querySelector('#poster').innerHTML += `<a href="#"><img src="${data.Search[index].Poster}"></a>`
+
+                    }
+
+
+                })
+        })
+
+        .catch(function (e) {
+            console.log('ERROR: ' + e.message);
+            
+
+        })
+
+})
