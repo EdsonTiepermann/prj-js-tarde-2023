de58debf41b4012b9118e5bccbfb5cf0aeaf95da
IagoGonçalves22 <iago.g.goncalves@gmail.com>
2023-10-27 22:35:05 -0300

    melhoria de apresentacao apos input buscar vazio

--- iago/searchPage.js
+++ iago/searchPage.js
@@ -7,21 +7,26 @@ function limparInput() {
 }
 
 function showData(listaFilme) {
-  document.querySelector("#inicial").style.display = "none";
-  document.getElementById("object").innerHTML = "";
+  console.log(listaFilme);
+  if (listaFilme.Response !== "False") {
+    document.querySelector("#inicial").style.display = "none";
+    document.getElementById("object").innerHTML = "";
 
-  for (let index = 0; index < listaFilme.Search.length; index++) {
-    document.getElementById(
-      "object"
-    ).innerHTML += `<img id="${listaFilme.Search[index].Title}" class="qtdf" src=${listaFilme.Search[index].Poster} alt="Minha Figura"></img>`;
-  }
-  var elementos = document.querySelectorAll(".qtdf");
+    for (let index = 0; index < listaFilme.Search.length; index++) {
+      document.getElementById(
+        "object"
+      ).innerHTML += `<img id="${listaFilme.Search[index].Title}" class="qtdf" src=${listaFilme.Search[index].Poster} alt="Minha Figura"></img>`;
+    }
+    var elementos = document.querySelectorAll(".qtdf");
 
-  [].forEach.call(elementos, function (el) {
-    el.addEventListener("click", function (evento) {
-      window.location.href = `./poster.html?nomeFilme=${el.id}`;
+    [].forEach.call(elementos, function (el) {
+      el.addEventListener("click", function (evento) {
+        window.location.href = `./poster.html?nomeFilme=${el.id}`;
+      });
     });
-  });
+  } else {
+    null;
+  }
 }
 
 document.querySelector("#buscar").addEventListener("click", function (e) {
