bf0641a48eeaaaf40888724a86d06c6e0ed01460
Gabriel <gabrielsantosgibson@gmail.com>
2023-11-06 14:20:28 -0300

    prj final

--- thalise_gabriel/pagfilme.js
+++ thalise_gabriel/pagfilme.js
@@ -1,40 +1,24 @@
-let getParametro = new URLSearchParams(window.location.search);
-let nomeFilme = getParametro.get("tituloFilme");
+let nomeParametro = new URLSearchParams(window.location.search);
+let tituloFilme = nomeParametro.get('titulo');
 
 const apikey = "cfc1236f";
 const options = {
-  method: "GET",
-  mode: "cors",
-  cache: "default",
+    method: "GET",
+    mode: "cors",
+    cache: "default"
 };
 
-fetch(`http://www.omdbapi.com/?t=${nomeFilme}&apikey=${apikey}`, options)
-.then( function (response) {
-    response.json()
-    .then(function (data) {
-      console.log(data);
-      document.querySelector("#titulo").innerHTML = `<p>${data.Title}</p>`;
-      document.querySelector("#nota").innerHTML = `<p>${data.Metascore}</p>`;
-      document.querySelector("#sinopse").innerHTML = `<p>${data.Plot}</p>`;
-      document.querySelector("#genero").innerHTML = `<p>${data.Genre}</p>`;
-      document.querySelector("#class").innerHTML = `<p>${escolhaClassi(data.Rated)}</p>`;
-      document.querySelector("#filmes").innerHTML = `<img src=${data.Poster}>`;
+fetch(`https://www.omdbapi.com/?t=${tituloFilme}&apikey=${apikey}`, options)
+    .then(response => response.json())
+    .then(data => {
+        if (data) {
+            console.log(data);
+            document.querySelector("#titulo").innerHTML = data.Title ? `<p>${data.Title}</p>` : '';
+            document.querySelector("#nota").innerHTML = data.Metascore ? `<p>${data.Metascore}</p>` : '';
+            document.querySelector("#sinopse").innerHTML = data.Plot ? `<p>${data.Plot}</p>` : '';
+            document.querySelector("#genero").innerHTML = data.Genre ? `<p>${data.Genre}</p>` : '';
+            if (data.Poster) {
+                document.querySelector("#filmes").innerHTML = `<img src="${data.Poster}" alt="${data.Title}">`;
+            }
+        }
     });
-
-    function escolhaClassi(classi) {
-      switch (classi) {
-          case "G":
-              return "Classificação Livre";
-          case "PG":
-              return "Orientação dos Responsáveis";
-          case "PG-13":
-              return "Classificação 14 anos";
-          case "R":
-              return "Classificação 16 anos";
-          case "NC-17":
-              return "Classificação 18 anos";
-          default:
-              return "Classificação não disponível";
-  }
-   }
- });
--- thalise_gabriel/pesquisa.js
+++ thalise_gabriel/pesquisa.js
@@ -1,47 +1,48 @@
-var filme = document.querySelector("#filme")
-const botao = document.querySelector("#botao")
-var titulo =  document.querySelector("#titulo")
+const filmeInput = document.querySelector("#filme");
+const botao = document.querySelector("#botao");
+const saudacao = document.querySelector('#saudacao');
+const lista = document.querySelector("div.lista");
 
 let nomeParametro = new URLSearchParams(window.location.search);
-let nomeCliente = nomeParametro.get('login')
+let nomeCliente = nomeParametro.get('login');
 
-document.querySelector('#saudacao').innerHTML = `Seja bem vindo ${nomeCliente}`;
+if (nomeCliente) {
+    saudacao.innerHTML = `Seja bem-vindo ${nomeCliente}`;
+}
 
 botao.addEventListener('click', function (e) {
-    let filmes = filme.value
-e.preventDefault()
-const options = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default',
-}
-fetch(`http://www.omdbapi.com/?s=${filmes}&apikey=cfc1236f`, options)
-.then(function (response) {
-    response.json()
-        .then(function (data) {
-            carregarFilme(data)
-                
-                               
-        })
-})
-
-.catch((error) => {
-    console.log(error);
+    e.preventDefault();
+    buscarFilmes(filmeInput.value);
 });
-})
 
-const carregarFilme = (json) => {
-    const lista = document.querySelector("div.lista");
+const buscarFilmes = (nomeFilme) => {
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+    };
+    
+    fetch(`https://www.omdbapi.com/?s=${encodeURIComponent(nomeFilme)}&apikey=cfc1236f`, options)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.Search) {
+                carregarFilmes(data);
+            } else {
+                alert("Nenhum filme encontrado")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+const carregarFilmes = (json) => {
     lista.innerHTML = "";
-
+    
     json.Search.forEach(element => {
-        console.log(element);
-
-       let item = document.createElement("div");
-       item.classList.add("item");
-
-       item.innerHTML = `<a href= pagfilme.html?titulo=${element.Title})> <img src="${element.Poster}" /><h2>${element.Title}</h2></a>`;
-
-       lista.appendChild(item);
+        let item = document.createElement("div");
+        item.classList.add("item");
+        item.innerHTML = `<a href="pagfilme.html?titulo=${encodeURIComponent(element.Title)}"> <img src="${element.Poster}" alt="${element.Title}" /><h2>${element.Title}</h2></a>`;
+        lista.appendChild(item);
     });
 }
