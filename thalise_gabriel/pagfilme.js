32fc6b0841a7904bee0c27df473d4258399b322d
gibsonsg002 <gabrielsantosgibson@gmail.com>
2023-11-06 17:20:50 -0300

    prj final

--- thalise_gabriel/pagfilme.js
+++ thalise_gabriel/pagfilme.js
@@ -1,24 +1,24 @@
-let nomeParametro = new URLSearchParams(window.location.search);
-let tituloFilme = nomeParametro.get('titulo');
+let filmeParametro = new URLSearchParams(window.location.search);
+let tituloFilme = filmeParametro.get('titulo');
 
-const apikey = "cfc1236f";
+
 const options = {
     method: "GET",
     mode: "cors",
     cache: "default"
 };
 
-fetch(`https://www.omdbapi.com/?t=${tituloFilme}&apikey=${apikey}`, options)
+fetch(`https://www.omdbapi.com/?t=${tituloFilme}&apikey=cfc1236f`, options)
     .then(response => response.json())
     .then(data => {
         if (data) {
             console.log(data);
             document.querySelector("#titulo").innerHTML = data.Title ? `<p>${data.Title}</p>` : '';
             document.querySelector("#nota").innerHTML = data.Metascore ? `<p>${data.Metascore}</p>` : '';
-            document.querySelector("#sinopse").innerHTML = data.Plot ? `<p>${data.Plot}</p>` : '';
+            document.querySelector("#plot").innerHTML = data.Plot ? `<p>${data.Plot}</p>` : '';
             document.querySelector("#genero").innerHTML = data.Genre ? `<p>${data.Genre}</p>` : '';
             if (data.Poster) {
-                document.querySelector("#filmes").innerHTML = `<img src="${data.Poster}" alt="${data.Title}">`;
+                document.querySelector("#poster").innerHTML = `<img src="${data.Poster}" alt="${data.Title}">`;
             }
         }
     });
