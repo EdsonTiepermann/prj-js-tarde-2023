44c4b1e94b5b8f91238b11f4e0690a1cc6b84969
joaofelipe02 <joaodecarvalhopradk@gmail.com>
2023-11-10 17:37:22 -0300

    prj 10/11/2023

--- joao_felipe/ParametroT.js
+++ joao_felipe/ParametroT.js
@@ -1,36 +1,30 @@
 
-let infFilmes = nameParameter.get("infFilmes");
-
-    let infoFilmes = filmeInfs.value;
-
-    const Options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    }
-
-    fetch(`http://www.omdbapi.com/?t=${infoFilmes}&apikey=eca49c5b`, Options)
-
-        .then(function(response) {
-            response.json()
-
-                .then(function (inf) {
-                    
-                    for (let index = 0; index < inf.Search.length; index++) {
-                        console.log(inf.Search[index])
-
-                        document.querySelector('#inf').innerHTML += `<a href="ParametroT.HTML"><img src="${data.Search[index].Inf}"></a>`
-                    }
-
-
-                })
-        })
-
-        .catch(function (e) {
-            console.log('ERROR: ' + e.message);
-            
-
-        })
+let prmtUrl = new URLSearchParams(window.location.search);
+let infoFilmes = prmtUrl.get("nomefilme");
 
 
 
+const Options = {
+  method: "GET",
+  mode: "cors",
+  cache: "default",
+};
+
+fetch(`http://www.omdbapi.com/?t=${infoFilmes}&apikey=eca49c5b`, Options)
+  .then(function (response) {
+    response
+      .json()
+
+      .then(function (rtnFilmes) {
+        console.log(rtnFilmes)
+
+
+
+          document.querySelector("#inf").innerHTML += `${data.Search[index].rtnFilmes}`
+        
+      });
+  })
+
+  .catch(function (e) {
+    console.log("ERROR: " + e.message);
+  });
