40ef31fb8de27c12ed6a24ba18a87e44c91f1b45
Cortelete <d.cortelete@gmail.com>
2023-11-12 15:25:59 -0300

    12/11

--- davi/login.js
+++ davi/login.js
@@ -1,50 +1,35 @@
-var login = document.querySelector('#login')
-var senha = document.querySelector('#senha')
-
-const submit = document.querySelector('#botao')
+var login = document.querySelector('#login');
+var senha = document.querySelector('#senha');
+const submit = document.querySelector('#botao');
 
 submit.addEventListener('click', function (e) {
-    e.preventDefault()
+    e.preventDefault();
     let searchl = login.value;
     let searchs = senha.value;
 
-    //alert("texto qualquer")
     const options = {
         method: 'get',
         mode: 'cors',
         cache: 'default'
-
-    }
+    };
 
     fetch(`ws/loginJson.json`, options)
         .then(function (response) {
-
-            response.json()
-
-                .then(function (data) {
-                    //console.log(data);
-                    for (let i = 0; i < data.length; i++) {
-                        let login1 = data[i].login;
-                        let senha1 = data[i].senha;
-                        let nome = data[i].nome;
-                       
-                        if (searchl == login1 && searchs == senha1) {
-                            //console.log(data[i].nome)
-                            window.location.assign(`sfilmes.html?nome=${nome}`);
-                            //location.href = `sfilmes.html?nome=${nome}`
-                        } else {
-                            alert('Dados Incorretos')
-                        }
-                    }
-
-
-
-
-                })
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                let login1 = data[i].login;
+                let senha1 = data[i].senha;
+                let nome = data[i].nome;
+                if (searchl === login1 && searchs === senha1) {
+                    window.location.assign(`sfilmes.html?nome=${nome}`);
+                    return;
+                }
+            }
+            alert('Dados Incorretos');
         })
         .catch(function (e) {
             console.log('Error: ' + e.message);
-        })
-
-})
-//window.open(`./filmes.html?nomeUsu=${variavelUsuario}&senha=${variavelSenha}`);
+        });
+});
--- davi/sfilmes.js
+++ davi/sfilmes.js
@@ -1,7 +1,74 @@
-let nomeparametros = new URLSearchParams(window.location.search);
-let nomeusuario = nomeparametros.get("nome");
+document.addEventListener('DOMContentLoaded', function () {
+    const nomeparametros = new URLSearchParams(window.location.search);
+    const nomeusuario = nomeparametros.get("nome");
 
-console.log(nomeusuario)
+    document.querySelector('#nomeTopo').innerHTML = `Seja Bem Vindo ${nomeusuario}`;
 
-document.querySelector('#nomeTopo').innerHTML = `Seja Bem Vindo ${nomeusuario}`
+    const form = document.querySelector('.formLogin');
 
+    if (form) {
+        const filmeInput = document.querySelector('#filme');
+        const resultsContainer = document.querySelector('#resultsContainer');
+
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            searchMovies();
+        });
+
+        function searchMovies() {
+            let buscafilme = filmeInput.value;
+            let type = 'movie';
+
+            console.log('Buscando filme:', buscafilme);
+
+            const options = {
+                method: 'get',
+                mode: 'cors',
+                cache: 'default'
+            };
+
+            fetch(`http://www.omdbapi.com/?s=${buscafilme}&type=${type}&apikey=f8e27f5f`, options)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    resultsContainer.innerHTML = '';
+
+                    if (data.Search) {
+                        data.Search.forEach(function (movie) {
+                            const movieCard = document.createElement('div');
+                            movieCard.classList.add('movie-card');
+
+                            const movieTitle = document.createElement('h2');
+                            movieTitle.textContent = movie.Title;
+
+                            const moviePoster = document.createElement('img');
+                            moviePoster.src = movie.Poster;
+
+                            movieCard.appendChild(movieTitle);
+                            movieCard.appendChild(moviePoster);
+                            resultsContainer.appendChild(movieCard);
+                        });
+
+                        document.body.classList.add('has-poster');
+                        
+                        document.querySelectorAll('.movie-card').forEach(function (card, index) {
+                            card.addEventListener('click', function () {
+                                const selectedMovie = data.Search[index];
+                                const queryString = `?title=${encodeURIComponent(selectedMovie.Title)}&poster=${encodeURIComponent(selectedMovie.Poster)}&rating=${encodeURIComponent(selectedMovie.Rated)}&genre=${encodeURIComponent(selectedMovie.Genre)}&plot=${encodeURIComponent(selectedMovie.Plot)}`;
+                                window.location.assign(`tfilmes.html${queryString}`);
+                            });
+                        });
+                    } else {
+                        console.log('Nenhum resultado encontrado.');
+                        resultsContainer.innerHTML = 'Nenhum resultado encontrado.';
+                    }
+                })
+                .catch(function (error) {
+                    console.error('Erro:', error.message);
+                });
+        }
+    } else {
+        console.error('Elemento .formLogin não encontrado.');
+    }
+});
